0456aceda8257462b0808bf0b78e304532ef8f7e
Mythic534 <tom.simpson733@gmail.com>
2025-11-28 11:34:59 +0000

    Clean results data

--- calculate_results.py
+++ calculate_results.py
@@ -17,7 +17,10 @@ for csv_file in DATA_DIR.iterdir():
     results.append(results_series)
 
 df = pd.concat(results, axis=1).fillna(0)
-df = df.round(4)
 df.columns = [f.stem for f in DATA_DIR.iterdir()]
+
+df = df.round(4)
+df = df.loc[df.sum(axis=1).sort_values(ascending=False).index]  # Sort by total score
+df = df.loc[(df != 0).any(axis=1)]  # Remove users with 0 score due to rounding
 
 df.to_csv(RESULTS_CSV)
